03bc3a4cf39787b1aa9db30d677c42b60095a9e5
Andres Babativa <carlos3k11@gmail.com>
2024-02-09 14:59:58 -0500

    feat: add styled-components as an external dependency

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react"],
+      external: ["react", "styled-components"],
       output: {
         globals: {
           react: "React",
