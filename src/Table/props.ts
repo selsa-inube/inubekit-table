0228b779505abbf183de80b91391ecff6a886239
cmarin001 <cmarin6051@gmail.com>
2024-07-04 11:36:31 -0500

    fix: update table component in order to follow figma design

--- src/Table/props.ts
+++ src/Table/props.ts
@@ -1,66 +1,43 @@
-interface IDataItem {
-  [key: string]: {
-    value: React.ReactNode;
-    type?: IActionType;
-    checked?: boolean;
-    onToggle?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    onClick?: (e: React.MouseEvent<HTMLTableCellElement>) => void;
-  };
-}
+const tableLayouts = ["auto", "fixed"] as const;
+type ITableLayout = (typeof tableLayouts)[number];
 
-const actionType = ["text", "toggle", "icon", "custom"] as const;
-type IActionType = (typeof actionType)[number];
+const borderCollapses = ["collapse", "separate"] as const;
+type ITableBorderCollapse = (typeof borderCollapses)[number];
 
-const alignContent = ["center", "left", "right"] as const;
-type ITableAlignContent = (typeof alignContent)[number];
+const borderStyles = ["solid", "dashed"] as const;
+type ITableBorderStyle = (typeof borderStyles)[number];
 
 const parameters = {
   docs: {
     description: {
       component:
-        "A versatile side navigation component, which allows you to nest links, from a full screen perspective",
+        "A versatile table component, which allows you to customize the layout, borders, and children.",
     },
   },
 };
 
 const props = {
-  caption: {
-    description: "A string to set the caption of the table.",
+  tableLayout: {
+    description: "Specifies the layout algorithm to be used for the table.",
+    control: { type: "select", options: tableLayouts },
+  },
+  borderCollapse: {
+    description: "Controls the table's border collapse behavior.",
+    control: { type: "select", options: borderCollapses },
+  },
+  borderWidth: {
+    description: "Defines the width of the table border.",
     control: { type: "text" },
   },
-  columns: {
-    description:
-      "(Array[objects]): Defines the columns of the table, each object contains the span and optional styles.",
+  borderStyle: {
+    description: "Defines the style of the table border.",
+    control: { type: "select", options: borderStyles },
+  },
+  children: {
+    description: "The content to display inside the table.",
     control: { type: "object" },
-  },
-  headers: {
-    description:
-      "(Array[objects]): Defines the headers of the table, each object contains the label, key, and optional action.",
-    control: { type: "object" },
-  },
-  pageLength: {
-    description: "(number): The number of entries to display per page.",
-    control: { type: "number" },
-  },
-  align: {
-    description:
-      "(string): Alignment of content in the table cells. Can be 'center', 'left', or 'right'.",
-    control: { type: "select", options: ["center", "left", "right"] },
-  },
-  data: {
-    description: "(Array[objects]): The data to display in the table.",
-    control: { type: "object" },
-  },
-  onToggle: {
-    description:
-      "(function): Callback function when a toggle action is performed.",
-    action: "onToggle",
-  },
-  onClick: {
-    description: "(function): Callback function when a cell is clicked.",
-    action: "onClick",
   },
 };
 
 export { parameters, props };
-export type { IActionType, ITableAlignContent, IDataItem };
+export type { ITableLayout, ITableBorderCollapse, ITableBorderStyle };
--- src/Table/styles.js
+++ src/Table/styles.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 import { inube } from "@inubekit/foundations";
 
 const StyledTable = styled.table`
-  border-collapse: collapse;
-  table-layout: fixed;
+  border-collapse: ${({ $borderCollapse }) => $borderCollapse};
+  table-layout: ${({ $tableLayout }) => $tableLayout};
   width: 100%;
   & > tfoot > tr {
     border: none;
@@ -11,8 +11,10 @@ const StyledTable = styled.table`
 `;
 
 const StyledTableContainer = styled.div`
-  border: 1px solid
-    ${({ theme }) => theme?.table?.border?.color || inube.table.border.color};
+  border: ${({ $borderWidth, $borderStyle, theme }) =>
+    `${$borderWidth} ${$borderStyle} ${
+      theme?.table?.border?.color || inube.table.border.color
+    }`};
   border-radius: 8px;
   overflow: hidden;
   width: 100%;
